9857f8118e194ab70be4f939b65fc795cae32ea0
PAVincius <vinicius.pratesaraujo@gmail.com>
2022-05-21 18:12:49 -0300

    :sparkles: feat: Hover and click animations

--- react_three_fiber/pages/index.js
+++ react_three_fiber/pages/index.js
@@ -2,18 +2,30 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Canvas } from 'react-three-fiber'
+import React, { useState } from 'react'
+
+const Sphere = () =>{
+
+  const [hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
+
+  return (
+    <mesh 
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      onClick={() => setActive(!active)}
+      scale={active  ? [2, 2, 2] : [1, 1, 1]}
+    >
+      <sphereBufferGeometry attach="geometry" args={[1, 16, 32]}/>
+      <meshBasicMaterial attach="material" color={hovered ? "gray" : "black"}/>
+    </mesh>
+  )
+}
 
 export default function Home() {
   return (
     <Canvas>
-        <mesh>
-          <sphereBufferGeometry 
-            attach="geometry" args={[2, 16, 16]}
-          />
-          <meshBasicMaterial 
-            attach="material" color="red"
-          />
-        </mesh>
+      <Sphere/>
     </Canvas>
   )
 }
